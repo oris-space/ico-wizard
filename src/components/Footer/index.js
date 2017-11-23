03bd39a306f5bd3b5b28e1b60b246a3f3eabd509
orisspace <32382857+orisspace@users.noreply.github.com>
2017-11-23 12:17:23 +0600

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 export const Footer = () => (
 	<footer className="footer">
 		<div className="container">
-			<p className="rights">2017 Oracles Network. All rights reserved.</p>
+			<p className="rights">2017 Oris.Space All rights reserved.</p>
       <Link className="logo" to='/'></Link>
 			<div className="socials">
-			  <a href="https://twitter.com/oraclesorg" className="social social_twitter"></a>
-        <a href="https://www.oracles.org" className="social social_oracles"></a>
-        <a href="https://t.me/oraclesnetwork" className="social social_telegram"></a>
-        <a href="https://github.com/oraclesorg/" className="social social_github"></a>
+			  <a href="https://twitter.com/AKolokhmatov" className="social social_twitter"></a>
+        <a href="https://orisspace.io" className="social social_oracles"></a>
+        <a href="https://t.me/orisspace" className="social social_telegram"></a>
+        <a href="https://github.com/oris-space" className="social social_github"></a>
 			</div>
 		</div>
 	</footer>
